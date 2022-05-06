8b1df5a4e39e50427a2848f57f051985f526512e
it20622114@my.sliit.lk <it20622114@my.sliit.lk>
2022-05-06 22:44:18 +0530

    implimenting add category

---
+++ app/src/main/java/com/example/bow_wow/AddCategoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.bow_wow;
+
+
+public class AddCategoryActivity {
+    private TextInputEditText categoryNameEdt,categoryDesEdt,categoryDateEdt,categoryImgEdt;
+    private Button addCategoryBtn;
+    private ProgressBar loadingPB;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private String categoryID;
+
+
+
+
+}
