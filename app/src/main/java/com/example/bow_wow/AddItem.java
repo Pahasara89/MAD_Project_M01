55cf364ef553c67b94dd8a40707fb22cd14aba33
IT20102050 <99387526+IT20102050@users.noreply.github.com>
2022-05-06 22:51:50 +0530

    implementing items

---
+++ app/src/main/java/com/example/bow_wow/AddItem.java
@@ -0,0 +1,16 @@
+package com.example.bow_wow;
+
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class AddItem extends AppCompatActivity {
+    private TextInputEditText ItemNameEdit,ItemPriceEdit,ItemCategoryEdit,ItemImageEdit,ItemLinkEdit,ItemDescriptionEdit;
+    private Button addItemBtn;
+    private ProgressBar loadingPB;
+
+    private String ItemID;
+}
