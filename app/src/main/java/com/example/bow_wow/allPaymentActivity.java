da6c5658b5a4d98b7be628b0eed69fadfb500625
Pahasara89 <pahasararashmika89@gmail.com>
2022-05-08 21:48:47 +0530

    implementing all payments

---
+++ app/src/main/java/com/example/bow_wow/allPaymentActivity.java
@@ -0,0 +1,21 @@
+package com.example.bow_wow;
+
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class allPaymentActivity {
+
+    private RecyclerView paymentRV;
+    private ProgressBar loadingPB;
+    private FloatingActionButton addFAB;
+    private RelativeLayout bottomSheetRL;
+
+
+
+}
