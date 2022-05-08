7db8c17b6ac3ed58cc392cf6b0534b55136f3d40
Pahasara89 <pahasararashmika89@gmail.com>
2022-05-08 22:10:58 +0530

    create successful page

---
+++ app/src/main/java/com/example/bow_wow/SuccessfulPage.java
@@ -0,0 +1,15 @@
+package com.example.bow_wow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SuccessfulPage {
+
+
+    private Button idBackHome;
+
+
+    }
+
