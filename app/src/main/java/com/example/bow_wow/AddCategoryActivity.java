2894a822fc3f2781989dcd391b8957963209b768
it20622114@my.sliit.lk <it20622114@my.sliit.lk>
2022-05-08 20:46:59 +0530

    edit add category activity

--- app/src/main/java/com/example/bow_wow/AddCategoryActivity.java
+++ app/src/main/java/com/example/bow_wow/AddCategoryActivity.java
@@ -2,14 +2,5 @@ package com.example.bow_wow;
 
 
 public class AddCategoryActivity {
-    private TextInputEditText categoryNameEdt,categoryDesEdt,categoryDateEdt,categoryImgEdt;
-    private Button addCategoryBtn;
-    private ProgressBar loadingPB;
-    private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference databaseReference;
-    private String categoryID;
-
-
-
 
 }
