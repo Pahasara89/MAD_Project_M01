d2c520dad58870378ddf2be71f3074b5c7bcae13
Pahasara89 <pahasararashmika89@gmail.com>
2022-05-08 17:22:04 +0530

    Add payment activity

---
+++ app/src/main/java/com/example/bow_wow/AddPaymentActivity.java
@@ -0,0 +1,19 @@
+package com.example.bow_wow;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class AddPaymentActivity {
+
+
+    private TextInputEditText paymentNameEdt, paymentPhoneEdt, paymentAddressEdt, paymentPlaceEdt, paymentCardEdt, paymentNoteEdt;
+    private Button addPaymentBtn;
+    private ProgressBar loadingPB;
+    private String paymentID;
+
+
+}
